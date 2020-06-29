e4032fe7a4b6db1f3e4f0cc847a7c51b19ec6381
Shaun King <kingy31@gmail.com>
2020-06-29 01:07:25 +0100

    added material design

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-products',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
-  }
+
+   }
 
 }
